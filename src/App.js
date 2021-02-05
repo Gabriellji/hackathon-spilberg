7aae9f17f555fc61309a2f976bd3aaec32b532e3
mancia08 <teikomancini1993@gmail.com>
2021-02-06 00:14:01 +0100

    basic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, {useContext, useEffect} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import {Switch, Route} from 'react-router-dom'
 import {Context} from './context/Context'
-import Navbar from './components/pages/Navbar'
 import Home from './components/pages/Home'
-import Contact from './components/pages/Contact'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   const context = useContext(Context)
-
+  const [open, setOpen] = useState(false);
   useEffect(()=>{console.log("placeholder")},[])
 
   return(
+
     <main>
       <Navbar/>
       <Switch>
         <Route exact path="/" render={() => <Home />} />
-        <Route path="/contact" render={() => <Contact />} />
+        <Route path="/user/wall" render={() => <Home />} />
+        <Route path="/user/swipe" render={() => <Home />} />
+        <Route path="/user/myideas" render={() => <Home />} />
+        <Route path="/user/newidea" render={() => <Home />} />
+        <Route path="/manager/wall" render={() => <Home />} />
+        <Route path="/manager/userprofile" render={() => <Home />} />
+        <Route path="/manager/favourites" render={() => <Home />} />
       </Switch>
     </main>
   )
--- src/context/Context.js
+++ src/context/Context.js
@@ -3,15 +3,16 @@ import React, { useState } from "react";
 export const Context = React.createContext();
 
 const Provider = ({ children }) => {
-  const [placeHolder, setPlaceholder] = useState("placeholder");
+  const [admin, setIsAdmin] = useState(false)
+
   const scrollTop = () => {
     window.scrollTo(0, 0);
   };
   return (
     <Context.Provider
       value={{
-        placeHolder,
-        setPlaceholder,
+        admin,
+        setIsAdmin,
         scrollTop
       }}
     >
