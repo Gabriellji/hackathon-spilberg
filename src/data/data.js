e54ff24eaea67ebe365609d4b0024910367ef2ea
mancia08 <teikomancini1993@gmail.com>
2021-02-05 21:49:30 +0100

    data

---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+export const managers = [
+  {
+    name: "Jacopo Luri",
+    favourite: [
+      /* put favourite comments here */
+    ],
+  },
+];
+
+export const user = [
+  {
+      id: "", /* uuidv */
+    username: "user", /* string */
+    password: "user", /* string */
+    name: "Naomi Carey", /* string */
+    position: "", /* string */
+    ideas: [
+      /* put array of ideas here */
+      {
+          id: "", /* uuidv */
+        question1: "",
+        question2: "",
+        question3: "",
+        totalLikes: [
+          /* likes other users gave. i want an array of name of users liking the comment  */
+        ],
+      },
+    ],
+  },
+];
