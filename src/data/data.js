f9bfadce2f9c4fbb23f45119e924571b8d29ca59
Gabriellji <gabriellji@yahoo.com>
2021-02-06 00:54:37 +0300

    feat: test data

---
+++ src/data/data.js
@@ -0,0 +1,163 @@
+export const managers = [
+  {
+    name: "Jacopo Luri",
+    favourite: [
+      /* put favourite comments here */
+    ],
+  },
+];
+
+export const user = [
+  {
+    id: 1 /* uuidv */,
+    username: "user" /* string */,
+    password: "user" /* string */,
+    name: "Naomi Carey" /* string */,
+    position: "" /* string */,
+    ideas: [
+      /* put array of ideas here */
+      {
+        id: "" /* uuidv */,
+        question1: "",
+        question2: "",
+        question3: "",
+        totalLikes: [
+          /* likes other users gave. i want an array of name of users liking the comment  */
+        ],
+      },
+    ],
+  },
+  {
+    id: 2 /* uuidv */,
+    username: "Peppe" /* string */,
+    password: "user" /* string */,
+    name: "Anna Salo" /* string */,
+    position: "" /* string */,
+    ideas: [
+      /* put array of ideas here */
+      {
+        id: "" /* uuidv */,
+        question1: "Lalala",
+        question2: "Yoooo",
+        question3: "Hahahaha",
+        totalLikes: [
+          /* likes other users gave. i want an array of name of users liking the comment  */
+        ],
+      },
+    ],
+  },
+  {
+    id: 3 /* uuidv */,
+    username: "Bear" /* string */,
+    password: "user" /* string */,
+    name: "Yahoo Karlos" /* string */,
+    position: "" /* string */,
+    ideas: [
+      /* put array of ideas here */
+      {
+        id: "" /* uuidv */,
+        question1: "Lalala",
+        question2: "Yoooo",
+        question3: "Hahahaha",
+        totalLikes: [
+          /* likes other users gave. i want an array of name of users liking the comment  */
+        ],
+      },
+    ],
+  },
+  {
+    id: 4 /* uuidv */,
+    username: "Bonito" /* string */,
+    password: "user" /* string */,
+    name: "Chowauwa" /* string */,
+    position: "" /* string */,
+    ideas: [
+      /* put array of ideas here */
+      {
+        id: "" /* uuidv */,
+        question1: "Lalala",
+        question2: "Yoooo",
+        question3: "Hahahaha",
+        totalLikes: [
+          /* likes other users gave. i want an array of name of users liking the comment  */
+        ],
+      },
+    ],
+  },
+  {
+    id: 5 /* uuidv */,
+    username: "Mary" /* string */,
+    password: "user" /* string */,
+    name: "Marry Poppins" /* string */,
+    position: "" /* string */,
+    ideas: [
+      /* put array of ideas here */
+      {
+        id: "" /* uuidv */,
+        question1: "Lalala",
+        question2: "Yoooo",
+        question3: "Hahahaha",
+        totalLikes: [
+          /* likes other users gave. i want an array of name of users liking the comment  */
+        ],
+      },
+    ],
+  },
+  {
+    id: 6 /* uuidv */,
+    username: "Silvester" /* string */,
+    password: "user" /* string */,
+    name: "Silvester Stallone" /* string */,
+    position: "" /* string */,
+    ideas: [
+      /* put array of ideas here */
+      {
+        id: "" /* uuidv */,
+        question1: "Lalala",
+        question2: "Yoooo",
+        question3: "Hahahaha",
+        totalLikes: [
+          /* likes other users gave. i want an array of name of users liking the comment  */
+        ],
+      },
+    ],
+  },
+  {
+    id: 7 /* uuidv */,
+    username: "Pizza" /* string */,
+    password: "user" /* string */,
+    name: "Naomi Carey" /* string */,
+    position: "" /* string */,
+    ideas: [
+      /* put array of ideas here */
+      {
+        id: "" /* uuidv */,
+        question1: "Lalala",
+        question2: "Yoooo",
+        question3: "Hahahaha",
+        totalLikes: [
+          /* likes other users gave. i want an array of name of users liking the comment  */
+        ],
+      },
+    ],
+  },
+  {
+    id: 8 /* uuidv */,
+    username: "Bob" /* string */,
+    password: "user" /* string */,
+    name: "Zheniya Habrykava" /* string */,
+    position: "" /* string */,
+    ideas: [
+      /* put array of ideas here */
+      {
+        id: "" /* uuidv */,
+        question1: "Lalala",
+        question2: "Yoooo",
+        question3: "Hahahaha",
+        totalLikes: [
+          /* likes other users gave. i want an array of name of users liking the comment  */
+        ],
+      },
+    ],
+  },
+];
