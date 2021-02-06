9e85db53f5020cf178eb9dbd53a478da7875d90a
JacopoLuri <geomjacopoluri@gmail.com>
2021-02-06 21:34:53 +0100

    search panel style done

--- src/components/pages/manager/search-panel/SearchPanel.js
+++ src/components/pages/manager/search-panel/SearchPanel.js
@@ -1,20 +1,39 @@
-import "./style.css";
+import React from "react";
+import styled from 'styled-components';
+import { theme } from "../../../../data/theme";
 
-const SearchPanel = ({ onClick, value, onChange }) => {
+import TopBtn from "../../../../components/Navbar/TopBtns"
 
-  return (
-    <div className="search_wrap">
-      <div className="search-inner_wrap">
-        <input
-          onChange={(e) => onChange(e)}
-          type="text"
-          placeholder="Employee name ..."
-          value={value}
-        />
-        <button>Search</button>
-      </div>
-    </div>
-  );
-};
+const StyledSearchPanel = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 8px;
+
+  input {
+    width: 100%;
+    border: 1px solid ${theme.color.orange};
+    border-radius: 10px;
+    line-height: 32px;
+    padding: 4px;
+  }
+
+  @media (min-width: 376px){
+    input {
+      max-width: 500px;
+    }
+  }
+`;
+
+const SearchPanel = ({ onClick, value, onChange }) => (
+  <StyledSearchPanel>
+    <input
+      onChange={(e) => onChange(e)}
+      type="text"
+      placeholder="Employee name ..."
+      value={value}
+    />
+    <TopBtn action={(e) => onClick(e)} text="Search" />
+  </StyledSearchPanel>
+);
 
 export default SearchPanel;
