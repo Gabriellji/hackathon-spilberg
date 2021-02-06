bc1438da97f8047a60890aab570c64572b7855da
Gabriellji <gabriellji@yahoo.com>
2021-02-06 18:15:43 +0300

    style: add basic style for employee list

--- src/components/pages/manager/employee-list/EmployeeList.js
+++ src/components/pages/manager/employee-list/EmployeeList.js
@@ -2,7 +2,7 @@ import { user } from "../../../../data/data";
 import UserProfile from "../user-profile/UserProfile";
 import React, { useState } from "react";
 
-import './style.css';
+import "./style.css";
 
 import Modal from "react-modal";
 
@@ -15,21 +15,27 @@ const EmployeeList = () => {
     setIsOpen(!isOpen);
   }
   return (
-    <div>
-      {user.map((employee) => (
-        <UserProfile name={employee.name} position={employee.position} onClick={toggleModal}/>
-      ))}
-      <Modal
-        isOpen={isOpen}
-        onRequestClose={toggleModal}
-        contentLabel="My dialog"
-        className="mymodal"
-        overlayClassName="myoverlay"
-        closeTimeoutMS={500}
-      >
-        <div>My modal dialog.</div>
-        <button onClick={toggleModal}>Close modal</button>
-      </Modal>
+    <div className="employee_wrap">
+      <div className="employee-inner_wrap">
+        {user.map((employee) => (
+          <UserProfile
+            name={employee.name}
+            position={employee.position}
+            onClick={toggleModal}
+          />
+        ))}
+        <Modal
+          isOpen={isOpen}
+          onRequestClose={toggleModal}
+          contentLabel="My dialog"
+          className="mymodal"
+          overlayClassName="myoverlay"
+          closeTimeoutMS={500}
+        >
+          <div>My modal dialog.</div>
+          <button onClick={toggleModal}>Close modal</button>
+        </Modal>
+      </div>
     </div>
   );
 };
--- src/components/pages/manager/user-profile/UserProfile.js
+++ src/components/pages/manager/user-profile/UserProfile.js
@@ -2,10 +2,9 @@ import "./style.css";
 
 const UserProfile = ({ name, position, onClick }) => {
   return (
-    <div className="user-profile_wrap">
+    <div className="user-profile_wrap" onClick={(e) => onClick(e)}>
       <p>{name}</p>
       <p>{position}</p>
-      <button onClick={(e) => onClick(e)}>Open modal</button>
     </div>
   );
 };
