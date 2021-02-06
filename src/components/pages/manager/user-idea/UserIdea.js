3dc21bdd0330c1bfd6d231b370cafafaf26bed5a
mancia08 <teikomancini1993@gmail.com>
2021-02-06 15:11:11 +0100

    homepage

--- src/components/pages/manager/user-idea/UserIdea.js
+++ src/components/pages/manager/user-idea/UserIdea.js
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 import { FaRegHeart } from "react-icons/fa";
 import { FaTrashAlt } from "react-icons/fa";
+import { GiPodiumWinner } from "react-icons/gi";
+import { GiPodiumSecond } from "react-icons/gi";
+import { GiPodiumThird } from "react-icons/gi";
 import "./style.css";
-
-const IconWrap = styled.div`
-  display: ${p => p.type === "fame" && "none"};
-`;
 
 const UserIdea = ({
   id,
@@ -18,17 +17,25 @@ const UserIdea = ({
   created,
   onClick,
   type,
+  rank,
 }) => {
   return (
     <div className="idea_wrap">
       <h1 className="total-likes_title">{totalLikes} Upvote</h1>
       <div className="idea-inner_wrap">
-        <IconWrap type={type} className="like_icon">
+        <div type={type} className="like_icon">
           <div className="icon-inner_wrap">
-            {type==="list" && <FaRegHeart id={id} onClick={e => onClick(e)} />}
-            {type==="favorite" && <FaTrashAlt id={id} onClick={e => onClick(e)} />}
+            {type === "list" && (
+              <FaRegHeart id={id} onClick={e => onClick(e)} />
+            )}
+            {type === "favorite" && (
+              <FaTrashAlt id={id} onClick={e => onClick(e)} />
+            )}
+            {rank == 0 && <GiPodiumWinner />}
+            {rank == 1 && <GiPodiumSecond />}
+            {rank == 2 && <GiPodiumThird />}
           </div>
-        </IconWrap>
+        </div>
         <h3>{title}</h3>
         <p>
           <span>Problem:</span>
