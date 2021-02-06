4699b2c395c01f1e66766a59ef4518de99ea4491
Gabriellji <gabriellji@yahoo.com>
2021-02-06 03:16:43 +0300

    style: add basic style to idea components

--- src/components/pages/manager/wall/user-idea/UserIdea.js
+++ src/components/pages/manager/wall/user-idea/UserIdea.js
@@ -1,9 +1,10 @@
+import './style.css';
 
 const UserIdea = ({name, title, question1, question2, question3, totalLikes, created}) => {
     return (
-        <div>
-        <h1>{totalLikes}</h1>
-        <div>
+        <div className="idea_wrap">
+        <h1 className="total-likes_title">{totalLikes} Upvote</h1>
+        <div className="idea-inner_wrap">
             <h3>{title}</h3>
             <p><span>Problem:</span>{question1}</p>
             <p><span>Idea:</span>{question2}</p>
