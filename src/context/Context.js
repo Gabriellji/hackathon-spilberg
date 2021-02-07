616a65fb70b11b914cd633b3f58fab2e7cd1b845
Gabriellji <gabriellji@yahoo.com>
2021-02-07 04:45:59 +0300

    feat: featch data method

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,25 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { user } from "../data/data.js";
 
 export const Context = React.createContext();
 
 const Provider = ({ children }) => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    fetch("http://localhost:5000/employee/users", {
+      method: "GET",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log(data);
+  }, []);
+
   const [admin, setIsAdmin] = useState(false);
   const newList = user;
   const [userList, setUserList] = useState(newList);
   const [state, setState] = useState("");
-  const likeHandler = e => {
-    let itemSelected = userList.find(user => user.id === e.target.id);
+  const likeHandler = (e) => {
+    let itemSelected = userList.find((user) => user.id === e.target.id);
 
     let copyOfItems = [...state];
     copyOfItems.push(itemSelected);
     setState(copyOfItems);
     const idx = userList.indexOf(itemSelected);
     userList.splice(idx, 1);
-    console.log(state)
+    console.log(state);
   };
-  const trashHandler = e => {
-    let element = state.find(el => el.id === e.target.id);
+  const trashHandler = (e) => {
+    let element = state.find((el) => el.id === e.target.id);
     const index = state.indexOf(element);
 
     let copyOfItems = [...state];
@@ -33,6 +54,7 @@ const Provider = ({ children }) => {
   return (
     <Context.Provider
       value={{
+        data,
         admin,
         setIsAdmin,
         scrollTop,
